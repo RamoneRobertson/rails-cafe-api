1db7511175219fe4bba50d824d5cd232829c67ad
Ramone Robertson <ramonerobertson10@gmail.com>
2024-12-23 13:40:42 +0900

    Finished create action for cafes controller

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -10,4 +10,19 @@ class Api::V1::CafesController < ApplicationController
     # Put the most recently created cafes first
     render json: @cafes.order(created_at: :desc)
   end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      render json: @cafe, status: :created
+    else
+      render json: { error: @cafe.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:title, :address, :picture, hours: {}, criteria: [])
+  end
 end
